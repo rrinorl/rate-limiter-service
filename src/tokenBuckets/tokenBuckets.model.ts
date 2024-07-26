08e5e2137094bdc933c2af1fa1b3fda4528a85cd
Ryan Stonebraker <rrinorl@users.noreply.github.com>
2024-07-26 11:38:14 -0400

    Fixed precision with rate limiter.

--- src/tokenBuckets/tokenBuckets.model.ts
+++ src/tokenBuckets/tokenBuckets.model.ts
@@ -38,7 +38,8 @@ async function takeTokens(bucketName: string, numTokens: number): Promise<TokenR
       `[tokenBuckets.model.takeTokens] Removing ${numTokens} token(s) from token bucket '${bucketName}' with ${tokenBucketStatus.tokens} token(s) remaining.`,
     );
     return {
-      tokensRemaining: tokenBucketStatus.tokens,
+      // responding with whole number tokens remaining without losing precision internally
+      tokensRemaining: Math.floor(tokenBucketStatus.tokens),
       tokensRemoved: numTokens,
     };
   }
@@ -47,7 +48,8 @@ async function takeTokens(bucketName: string, numTokens: number): Promise<TokenR
     `[tokenBuckets.model.takeTokens] Not enough tokens left for '${bucketName}' to remove ${numTokens} token(s) as ${tokenBucketStatus.tokens} token(s) remaining.`,
   );
   return {
-    tokensRemaining: tokenBucketStatus.tokens,
+    // responding with whole number tokens remaining without losing precision internally
+    tokensRemaining: Math.floor(tokenBucketStatus.tokens),
     tokensRemoved: 0,
   };
 }
@@ -57,8 +59,7 @@ function refillBucket(tokenBucketStatus: TokenBucketStatus) {
   const now = Date.now();
   const timePassedSec = (now - lastRefilledDate) / 1000;
   const tokensAddedPerSec = sustained / 60;
-  // NOTE: there may be a slight precision loss here via the floor function as it's rounding down.
-  const tokensToAdd = Math.floor(timePassedSec * tokensAddedPerSec);
+  const tokensToAdd = timePassedSec * tokensAddedPerSec;
 
   if (tokensToAdd > 0) {
     tokenBucketStatus.tokens = Math.min(tokens + tokensToAdd, burst);
@@ -69,7 +70,7 @@ function refillBucket(tokenBucketStatus: TokenBucketStatus) {
 interface TokenRemovalResponse {
   /** Tokens removed from bucket **/
   tokensRemoved: number;
-  /** Tokens remaining in bucket **/
+  /** Tokens remaining in bucket rounded down to a whole number **/
   tokensRemaining: number;
 }
 
@@ -80,7 +81,7 @@ interface TokenBucketStatus {
   burst: number;
   /** The number of sustained requested per minute. **/
   sustained: number;
-  /** Tokens left in bucket **/
+  /** Tokens left in bucket, including partial tokens **/
   tokens: number;
   /** Bucket last refilled date (milliseconds since epoch) **/
   lastRefilledDate: number;
